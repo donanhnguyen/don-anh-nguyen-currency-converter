19f1a3e36cfab3ab76e7d1a9daefe6a80202e3d4
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-02-20 17:20:57 -0600

    api key doesnt work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  const BASE_URL = "https://api.exchangeratesapi.io/v1/latest";
+  
+  const API_KEY = "AQ8q8j3ihr4OWpQ6Bj9CXrF14AaV9SdY";
+
+  function params() {
+    return new URLSearchParams({
+      access_key: "WNiS9hiaDj828hcgupjd14r0Xees78WR",
+      base: "USD",
+      
+    })
+  }
+
+  useEffect(() => {
+
+    // Axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.min.json`)
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+
+    fetch(`${BASE_URL}?${params()}`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>this is going to be a currency converter app</h1>
+
+        <h1>Currency Converter, by Don Anh Nguyen</h1>
+
+        <CurrencyRow />
+        <div>=</div>
+        <CurrencyRow />
+
       </header>
     </div>
   );
---
+++ src/CurrencyRow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function CurrencyRow() {
+
+
+  return (
+    <div>
+        <input type='number'></input>
+            <select>
+                <option>sex</option>
+            </select>
+    </div>
+  )
+}
