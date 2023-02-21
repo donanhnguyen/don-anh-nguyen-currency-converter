35c8bb3e475df104316790ed9bb489ff53e6c9bb
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-02-21 17:05:55 -0600

    almost done, leaving off at changing currencies and the second useEffect call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 import { useState, useEffect } from 'react';
-import Axios from 'axios';
 
 function App() {
 
-  const BASE_URL = "https://api.exchangeratesapi.io/v1/latest";
-  
-  const API_KEY = "AQ8q8j3ihr4OWpQ6Bj9CXrF14AaV9SdY";
+ 
 
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [currencySymbols, setCurrencySymbols] = useState();
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountIsFromCurrency, setAmountIsFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountIsFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    fromAmount = amount / exchangeRate;
+    toAmount = amount;
+  }
+
+  const API_KEY = "WNiS9hiaDj828hcgupjd14r0Xees78WR";
   var myHeaders = new Headers();
   myHeaders.append("apikey", API_KEY);
   var requestOptions = {
@@ -19,11 +33,55 @@ function App() {
 
   useEffect(() => {
 
+      // get rates
     fetch("https://api.apilayer.com/exchangerates_data/latest?&base=USD", requestOptions)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        const baseCurrency = Object.keys(data.rates)[0];
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(baseCurrency)
+        setExchangeRate(data.rates[baseCurrency])
+      })
 
+
+      // get symbols
+    fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        setCurrencySymbols(result.symbols)
+      })
+  
   }, [])
+  
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${toCurrency}&from=${fromCurrency}&amount=${amount}`, requestOptions)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+        })
+    }
+  }, [fromCurrency, toCurrency])
+
+  function changeFromCurrency (e) {
+    setFromCurrency(e.target.value)
+  }
+
+  function changeToCurrency (e) {
+    setToCurrency(e.target.value)
+  }
+
+  function handleChangeTheFromAmount (e) {
+    setAmount(e.target.value);
+    setAmountIsFromCurrency(true);
+  }
+
+  function handleChangeTheToAmount (e) {
+    setAmount(e.target.value);
+    setAmountIsFromCurrency(false);
+  }
+
 
   return (
     <div className="App">
@@ -31,9 +89,29 @@ function App() {
 
         <h1>Currency Converter, by Don Anh Nguyen</h1>
 
-        <CurrencyRow />
+        <h1>from</h1>
+        <CurrencyRow 
+          amount={fromAmount} 
+          onChangeCurrency={changeFromCurrency} 
+          selectedCurrency={fromCurrency} 
+          currencyOptions={currencyOptions} 
+          currencySymbols={currencySymbols}
+          onChangeAmount={handleChangeTheFromAmount}
+          />
+          
+
         <div>=</div>
-        <CurrencyRow />
+
+        <h1>to</h1>
+        <CurrencyRow 
+          amount={toAmount} 
+          onChangeCurrency={changeToCurrency} 
+          selectedCurrency={toCurrency} 
+          currencyOptions={currencyOptions} 
+          currencySymbols={currencySymbols}
+          onChangeAmount={handleChangeTheToAmount}
+          />
+
 
       </header>
     </div>
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-export default function CurrencyRow() {
+export default function CurrencyRow (props) {
+
+  const {currencyOptions, currencySymbols, selectedCurrency, onChangeCurrency, amount, onChangeAmount} = props;
 
 
+   
   return (
     <div>
-        <input type='number'></input>
-            <select>
-                <option>sex</option>
+        <input type='number' value={amount} onChange={onChangeAmount}></input>
+            <select value={selectedCurrency} onChange={onChangeCurrency}>
+              {currencyOptions.map((option, index) => {
+                return <option key={option + index} value={option}>{option}</option>
+              })}
             </select>
     </div>
   )
